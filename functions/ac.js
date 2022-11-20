7402a15e9fd2d7b6d61a7f787c6b220f2980ab78
Kareem-Khaled <kareemkhaled143@gmail.com>
2022-11-20 17:00:25 +0200

    edit

--- functions/ac.js
+++ functions/ac.js
@@ -10,12 +10,15 @@ const { JSDOM } = jsdom;
 
 // to request the pgae
 // const axios = require('axios');
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 
 // to remove extra's
 function strip(string) {
-  return string.replace(/^\s+|\s+$/g, '');
+    return string.replace(/^\s+|\s+$/g, '');
 }
+
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
 // to get the dom
 async function getDom(url) {
@@ -24,111 +27,115 @@ async function getDom(url) {
     //     const dom = new JSDOM(res.data);
     //     return dom.window.document;
     // })
-    const browser = await puppeteer.launch();
+    let browser = await puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+    });
     const page = await browser.newPage();
-    await page.goto(url, {waitUntil: 'domcontentloaded'});
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
     let data = await page.content();
     await browser.close();
     const dom = new JSDOM(data);
     return dom.window.document;
 }
+    // get ac solutions for each contestant
+    const getAc = async (url) => {
+        try {
+            const dom = await getDom(url);
+            let standing = dom.querySelector('.standings');
+            let problemsA = standing.rows[0].querySelectorAll('a');
 
-// get ac solutions for each contestant
-const getAc = async(url) => {
-    try{
-        const dom = await getDom(url);
-        let standing = dom.querySelector('.standings');
-        let problemsA = standing.rows[0].querySelectorAll('a');
-
-        let problems = [];
-        for (let problem of problemsA){
-            problems.push({
-                'name': problem.title, 
-                'link': 'https://codeforces.com' + problem.href
-            });
-        }
-        
-        let sheetNameA = dom.querySelector(".contest-name").querySelector('a');
-        let contest = {
-            'name': strip(sheetNameA.textContent),
-            'link': 'https://codeforces.com' + sheetNameA.href,
-            'problems': problems
-        };
-
-        let data = {};
-        for(let i = 1; i < standing.rows.length - 1; i++){
-            let team = 'Not a team', contestants = [];
-            let tr = standing.rows[i].querySelectorAll('td'), isTeam = true;
-            try{
-                trA = tr[1].querySelector('span').querySelectorAll('a');
+            let problems = [];
+            for (let problem of problemsA) {
+                problems.push({
+                    'name': problem.title,
+                    'link': 'https://codeforces.com' + problem.href
+                });
             }
-            catch{
-                isTeam = false;
-            }
-            if (isTeam && trA[0].href.includes('team')){ // it's a team
-                team = trA[0]['title'];
-                for (let k = 1; k < trA.length; k++){
-                    tmp = (trA[k].title.split(' '));
+
+            let sheetNameA = dom.querySelector(".contest-name").querySelector('a');
+            let contest = {
+                'name': strip(sheetNameA.textContent),
+                'link': 'https://codeforces.com' + sheetNameA.href,
+                'problems': problems
+            };
+
+            let data = {};
+            for (let i = 1; i < standing.rows.length - 1; i++) {
+                let team = 'Not a team', contestants = [];
+                let tr = standing.rows[i].querySelectorAll('td'), isTeam = true;
+                try {
+                    trA = tr[1].querySelector('span').querySelectorAll('a');
+                }
+                catch {
+                    isTeam = false;
+                }
+                if (isTeam && trA[0].href.includes('team')) { // it's a team
+                    team = trA[0]['title'];
+                    for (let k = 1; k < trA.length; k++) {
+                        tmp = (trA[k].title.split(' '));
+                        contestants.push(tmp[1]);
+                    }
+                }
+                else { // it's a contestant 
+                    tmp = (tr[1].querySelector('a').title.split(' '));
                     contestants.push(tmp[1]);
                 }
-            }
-            else{ // it's a contestant 
-                tmp = (tr[1].querySelector('a').title.split(' '));
-                contestants.push(tmp[1]);
-            }
 
-            let tds = standing.rows[i].querySelectorAll('td');
-            for(let i = 4; i < tds.length; i++){
-                let txt = strip(tds[i].querySelector('span').textContent) || '-';
-                if(txt[0] == '-') continue;
-                for(let j = 0; j < contestants.length; j++){
-                    if(!(contestants[j] in data)){ // new contestant to the data
-                        data[contestants[j]] = [];
+                let tds = standing.rows[i].querySelectorAll('td');
+                for (let i = 4; i < tds.length; i++) {
+                    let txt = strip(tds[i].querySelector('span').textContent) || '-';
+                    if (txt[0] == '-') continue;
+                    for (let j = 0; j < contestants.length; j++) {
+                        if (!(contestants[j] in data)) { // new contestant to the data
+                            data[contestants[j]] = [];
+                        }
+                        let pNum = problems[i - 4].name.split(' - ')[0];
+                        if (!data[contestants[j]].includes(pNum))
+                            data[contestants[j]].push(pNum);
                     }
-                    let pNum = problems[i - 4].name.split(' - ')[0];
-                    if(!data[contestants[j]].includes(pNum))
-                        data[contestants[j]].push(pNum);
                 }
             }
-        }
 
-        let keys = Object.keys(data);
-        keys.forEach(async (key) => {
-            data[key] = {
-                ac: data[key].join('-')
+            let keys = Object.keys(data);
+            keys.forEach(async (key) => {
+                data[key] = {
+                    ac: data[key].join('-')
+                }
+            });
+
+            return {
+                'status': 'OK',
+                'result': {
+                    'contest': contest,
+                    'contestants': data
+                },
             }
-        });
-
-        return {
-            'status': 'OK',
-            'result': {
-                'contest': contest,
-                'contestants': data
-            },
+        }
+        catch (err) {
+            return {
+                'status': 'FAILED',
+                'result': 'There is something wrong :(',
+                'err': err.message
+            }
         }
     }
-    catch (err){
-        return {
-            'status': 'FAILED',
-            'result': 'There is something wrong :(',
-            'err' : err.message
-        }
-    }
-}
 
-router.get('/g/:groupId/c/:contestId/p/:page', async (req, res) =>{
-    let {groupId, contestId, page} = req.params;
-    url = `https://codeforces.com/group/${groupId}/contest/${contestId}/standings/page/${page}?showUnofficial=true`
-    let ret = await getAc(url);
-    res.status(200).send(ret);
-})
+    router.get('/g/:groupId/c/:contestId/p/:page', async (req, res) => {
+        let { groupId, contestId, page } = req.params;
+        url = `https://codeforces.com/group/${groupId}/contest/${contestId}/standings/page/${page}?showUnofficial=true`
+        let ret = await getAc(url);
+        res.status(200).send(ret);
+    })
 
-router.get('/g/:groupId/c/:contestId/p/:page/l/:listId', async (req, res) =>{
-    let {groupId, contestId, listId, page} = req.params;
-    url = `https://codeforces.com/group/${groupId}/contest/${contestId}/standings/page/${page}?list=${listId}&showUnofficial=true`
-    let ret = await getAc(url);
-    res.status(200).send(ret);
-})
+    router.get('/g/:groupId/c/:contestId/p/:page/l/:listId', async (req, res) => {
+        let { groupId, contestId, listId, page } = req.params;
+        url = `https://codeforces.com/group/${groupId}/contest/${contestId}/standings/page/${page}?list=${listId}&showUnofficial=true`
+        let ret = await getAc(url);
+        res.status(200).send(ret);
+    })
 
-app.use('/.netlify/functions/ac', router);
-module.exports.handler = serverless(app);
+    app.use('/.netlify/functions/ac', router);
+    module.exports.handler = serverless(app);
